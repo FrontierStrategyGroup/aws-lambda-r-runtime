e3a60419e5097b6b73ebd3361daadbe4a97be45f
Andrew Robbins <arobbins@frontierstrategygroup.com>
2019-03-14 13:29:48 -0400

    no message

--- build_runtime_and_publish.sh
+++ build_runtime_and_publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=${VERSION:=3.5.1}
+VERSION=$1
 
 ./build_runtime.sh $VERSION
-aws lambda publish-layer-version --layer-name r-runtime --zip-file fileb://runtime.zip
+aws s3 cp runtime.zip s3://hydra.frontierview.com/
